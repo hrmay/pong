1bcce4262a9e4891d32dda4225e90761f929aaf8
RomanNumeralII <RomanNumeralII@gmail.com>
2016-01-12 19:59:29 -0500

    Made a simple pong game in unity using a tutorial

---
+++ Assets/BallScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+	public float speed = 30;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().velocity = Vector2.right * speed;
+	}
+
+	float HitFactor (Vector2 ballPos, Vector2 racketPos, float racketHeight) {
+		// ascii art:
+		// ||  1 <- at the top of the racket
+		// ||
+		// ||  0 <- at the middle of the racket
+		// ||
+		// || -1 <- at the bottom of the racket
+
+		return (ballPos.y - racketPos.y) / racketHeight;
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		// Note: 'col' holds the collision information. If the
+		// Ball collided with a racket, then:
+		//   col.gameObject is the racket
+		//   col.transform.position is the racket's position
+		//   col.collider is the racket's collider
+
+		if (col.gameObject.name == "RacketLeft") {
+			float y = HitFactor (transform.position, col.transform.position, col.collider.bounds.size.y);
+
+			Vector2 dir = new Vector2 (1, y).normalized;
+
+			GetComponent<Rigidbody2D> ().velocity = dir * speed;
+		}
+
+		if (col.gameObject.name == "RacketRight") {
+			float y = HitFactor (transform.position, col.transform.position, col.collider.bounds.size.y);
+
+			Vector2 dir = new Vector2 (-1, y).normalized;
+
+			GetComponent<Rigidbody2D> ().velocity = dir * speed;
+		}
+
+	}
+		
+}
---
+++ Assets/MoveRacket.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRacket : MonoBehaviour {
+	public float speed = 30;
+	public string axis = "Vertical";
+
+	// Use this for initialization
+	void FixedUpdate () {
+		float v = Input.GetAxisRaw (axis);
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, v) * speed;
+
+	}
+}
